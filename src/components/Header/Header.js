ad63ea5f9543e94fd33ce8a569e615e2a0234fda
Maycon Vieira <maycon-big@hotmail.com>
2023-03-02 15:14:29 -0300

    feat: first commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.scss';
+
+function Header() {
+    return(
+        <header>
+            <h1 className='h1'>Vamo filho</h1>
+        </header>
+    );
+}
+
+export default Header;
