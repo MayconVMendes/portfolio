cc174b89459696a7648c02d19d22eaaeac5e9985
Maycon Vieira <maycon-big@hotmail.com>
2023-03-02 20:53:27 -0300

    feat: create simple menu hamburguer and Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,43 @@
-import './Header.scss';
+import { useState } from "react";
+import "./Header.scss";
 
-function Header() {
-    return(
-        <header>
-            <h1 className='h1'>Vamo filho</h1>
-        </header>
-    );
+export default function Header() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        Maycon Mendes
+      </a>
+      <button
+        className="hamburger"
+        onClick={() => {
+          setIsExpanded(!isExpanded);
+        }}
+      >
+        <div className={isExpanded ? "container change" : "container"}>
+          <div class="bar1"/>
+          <div class="bar2"/>
+          <div class="bar3"/>
+        </div>
+      </button>
+      <div
+        className={
+          isExpanded ? "navigation-menu expanded" : "navigation-menu"
+        }
+      >
+        <ul>
+          <li>
+            <a href="/">Pagina inicial</a>
+          </li>
+          <li>
+            <a href="/">Sobre mim</a>
+          </li>
+          <li>
+            <a href="/">Contato</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
-export default Header;
